73363ba23c013b9962a64a1a05381240cde3288f
James <>
2021-07-13 05:06:58 -0400

    #1.init

--- lb-a/pages/index.vue
+++ lb-a/pages/index.vue
@@ -5,13 +5,20 @@
       <h1 class="title">
         lb-a
       </h1>
+
       <ul>
         <li>run default nuxtjs</li>
+
         <li>configure nuxt.config.js, add  server: {
            port: process.env.PORT || 3000 // default: localhost
         }</li>
+
         <li>Heroku Settings: HOST 0.0.0.0</li>
+
+        <li>hapi-api ... install npm install --save-dev @babel/preset-react in package.json</li>
+        <li>.babelrc ... "presets": ["@babel/preset-react","@babel/preset-env"]</li>
       </ul>
+
       <div class="links">
         <a
           href="https://nuxtjs.org/"
